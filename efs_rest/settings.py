84d14e20f5b61f58887adef3681e1eaaf8196996
ksabnekar <54640989+ksabnekar@users.noreply.github.com>
2021-03-18 12:20:00 -0500

    Update settings.py

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -131,8 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
+
 LOGIN_REDIRECT_URL = '/'
 
 
@@ -165,4 +170,6 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
 
